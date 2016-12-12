e9fc74e8a1c892750783383018163286409e15a4
Malik Harrison <harrmalik@gmail.com>
2016-12-12 11:44:26 -0500

    new db

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var users = require('./routes/users')(passport);
 var api =  require("./routes/api");
 
 var MongoStore = connectMongo(session);
-mongoose.connect('mongodb://admin-mh:password@ds033317.mongolab.com:33317/heroku_l7nbt7vw');
-// mongoose.connect('mongodb://localhost:27017/goingdowndb');
+mongoose.connect('mongodb://admin-mh:password@ds133158.mlab.com:33158/heroku_0mlbs4kj');
+//mongoose.connect('mongodb://localhost:27017/goingdowndb');
 var app = express();
 
 // view engine setup
